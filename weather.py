f44ac08e81c27f1055c29235a30552eeec715bc5
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-11-11 17:16:38 +0100

    working on saving data to json file

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 import datetime
+import json
+from pathlib import Path
 
 #https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
 
@@ -13,9 +15,10 @@ class Weather_info:
         self.date = date
         self.location = 'Warsaw'
         self.info = self.get_info()
+        self.logs_path = '.weather_logs.json'
         
     def get_info(self):
-        self.url_parameters = '&include=current&elements=precip,datetime,precipprob'
+        self.url_parameters = '&include=current&elements=precip,datetime,precipprob&unitGroup=metric'
         request_url = f'{self.BASE_URL}/{self.location}/{self.date}?key={self.api_key}{self.url_parameters}'
         r = requests.get(request_url)
         info = r.json()
@@ -27,12 +30,29 @@ class Weather_info:
         return self.get_rain_chance(precip)
 
     def get_rain_chance(self, precip):
-        if precip > 0.0:
-            return 'Będzie padać.'
-        elif precip == 0.0:
+        if precip == 0.0:
             return 'Nie będzie padać.'
+        elif precip > 0.0:
+            return 'Będzie padać.'
         else:
             return 'Nie wiem.'
+
+    def check_if_file_exists(self):
+        return Path.is_file(self.logs_path)
+
+    def check_data_in_file(self):
+        if self.check_if_file_exists == True:
+            with open(self.logs_path) as weather_data:
+                data = json.load(self.logs_path)
+                if self.date in data.keys():
+                    return data.get(self.date)
+                else:
+                    self.get_info()
+        else:
+            self.get_info()
+
+    def save_data_to_file(self):
+        pass
 
 if len(sys.argv) == 2:
     weather = Weather_info(api_key=sys.argv[1])
